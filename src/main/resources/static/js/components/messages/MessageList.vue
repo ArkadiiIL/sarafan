d65d65cd6e16c3cd91fd6db82174873de590b9cc
ArkadiiIl <odinls91@gmail.com>
2020-05-12 21:47:13 +0200

    Vuex added

--- src/main/resources/static/js/components/messages/MessageList.vue
+++ src/main/resources/static/js/components/messages/MessageList.vue
@@ -1,22 +1,19 @@
 <template>
     <v-layout align-space-around justify-start column>
-        <message-form :messages="messages" :messageAttr="message"/>
+        <message-form :messageAttr="message"/>
         <message-row v-for="message in sortedMessages"
                      :key="message.id"
                      :message="message"
-                     :editMessage="editMessage"
-                     :deleteMessage="deleteMessage"
-                     :messages="messages" />
+                     :editMessage="editMessage" />
     </v-layout>
 
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
     import MessageRow from "./MessageRow.vue";
     import MessageForm from "./MessageForm.vue";
-    import messagesApi from 'api/messages.js'
     export default {
-        props: ['messages'],
         data()
         {
             return{
@@ -27,24 +24,10 @@
             MessageRow,
             MessageForm
         },
-        computed:{
-            sortedMessages(){
-                return this.messages.sort((a,b) => -(a.id - b.id));
-            }
-
-        },
+        computed: mapGetters(['sortedMessages']),
         methods: {
             editMessage(message) {
                 this.message = message;
-            },
-            deleteMessage(message) {
-                    messagesApi.remove(message.id).then(result => {
-                        if (result.ok) {
-                            if(this.messages.indexOf(message) > -1) {
-                                this.messages.splice(this.messages.indexOf(message), 1);
-                            }
-                        }
-                    })
             }
         }
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue';
+import store from "./store/store.js";
 import {connect} from "./util/ws.js";
 import 'vuetify/dist/vuetify.min.css'
 
@@ -14,6 +16,7 @@ Vue.use(Vuetify);
 
 new Vue({
     vuetify : new Vuetify(),
+    store,
     el: '#app',
     render: a => a(App)
 });
